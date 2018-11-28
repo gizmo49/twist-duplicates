e384a79aa5a9c49abfabae176280249810e7b4b7
gizmo49 <chikacheezy@gmail.com>
2018-11-28 08:49:43 +0000

    recent changes

--- videos/urls.py
+++ videos/urls.py
@@ -7,6 +7,8 @@ from .import views
 
 urlpatterns = [
     path('', views.showvideo),
+    path('comparevid', views.compare_videofiles, name="comparevids"),
+
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
--- videos/views.py
+++ videos/views.py
@@ -1,20 +1,30 @@
+import uuid
+# import cv2  
+# import numpy as np
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Video
 from .forms import UploadFileForm
 from django.http import HttpResponseRedirect
 
+def compare_videofiles(request):
+    if request.method == 'POST':
+        return HttpResponse('cap')
+
 def handle_uploaded_file(f, filename):
-    with open('media/uploads/' + filename, 'wb+') as destination:
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    with open('static/uploads/' + filename, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+        return ('../static/uploads/' + filename)
 
 def showvideo(request):
    if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
-            return HttpResponse("success")
+            path_res = handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+            return render(request, 'home.html', {'form': form, 'path_res':path_res})    
         else:
             form = UploadFileForm()
         
